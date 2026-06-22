f26e76ad84b14d4ba5e65f45325184c7b2373d51
PocoBaBa <mechengmika@gmail.com>
2026-06-22 15:30:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -29,53 +29,6 @@ except Exception as e:
 if data_loaded:
     dino_names = df_dinos['Name'].tolist()
     
-    # Fetch save coordinates
-   # saved_log = local_storage.getItem("pot_logout_locations")
-   # if saved_log is None:
-    #    saved_log = {name: "" for name in dino_names}
-  #  else:
-        # Ensures new dinos get their own blank row later
-     #   for name in dino_names:
-       #     if name not in saved_log:
-        #        saved_log[name] = ""
-
-    # Logout Tracker
- #   with st.expander("📝 Show Logout Location Tracker"):
-     #   st.markdown("*Type your logout location.*")
-
-     #   log_data = pd.DataFrame({
-       #     "Dinosaur": dino_names,
-        #    "Last Logged Location": [saved_log.get(name, "") for name in dino_names]
-     #   })
-
-      #  edited_df = st.data_editor(
-        #    log_data,
-        #    column_config={
-         #       "Dinosaur": st.column_config.TextColumn(disabled=True),
-         #       "Last Logged Location": st.column_config.TextColumn(width="large")
-         #   },
-         #   disabled=False,
-         #   num_rows="fixed",
-          #  hide_index=True,
-          #  use_container_width=True
-       # )
-
-        # Check for new content
-     #   updated_log = {}
-     #   has_changes = False
-      #  for idx, row in edited_df.iterrows():
-      #      dino_name = row['Dinosaur']
-       #     new_loc = str(row['Last Logged Location']).strip()
-       #     updated_log[dino_name] = new_loc
-       #     if saved_log.get(dino_name) != new_loc:
-       #         has_changes = True
-
-      #  if has_changes:
-      #      local_storage.setItem("pot_logout_locations", updated_log)
-       #     st.rerun()
-
- #   st.markdown("---")
-    
     # --- SEARCH & SELECTION INTERFACE ---
     search_query = st.text_input("🔍 Search Dinosaurs...", "").strip()
 
